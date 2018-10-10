47e48663ba30d11a72c16a42222f2560ec178fcc
Truija99 <truijam@gmail.com>
2018-10-10 14:02:29 +0300

    Lisatud kujundus, lihtsustatud nupp.

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ let gramOutput = document.getElementById('gramOutput');
 let poundOutput = document.getElementById('poundOutput');
 let ozOutput = document.getElementById('ozOutput');
 let output = document.getElementById('output');
+let nightToggle = document.getElementById('nightToggle');
+let body = document.querySelector('body');
 
 output.style.visibility = 'hidden';
 
-
- // Listening to events
+// Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
- 
-    output.style.visibility = (kg === '0') ? 'hidden' : 'visible';  /* saab lühemalt if else lause kirjutada */
+    
+    output.style.visibility = (kg === '0') ? 'hidden' : 'visible';
+    
+    gramOutput.innerHTML = `${kg * 1000} g`;
+    poundOutput.innerHTML = `${(kg * 2.20462262).toFixed(2)} lb`
+    ozOutput.innerHTML = `${(kg * 35.2739619).toFixed(2)} oz`
+});
 
-     gramOutput.innerHTML = `${kg * 1000} g`;
-     poundOutput.innerHTML = `${(kg * 2.20462262).toFixed(2)} lb`;
-     ozOutput.innerHTML = `${(kg * 35.2739619).toFixed(2)} oz`; /* toFixed - saab komakohtasid ümardada pmst*/ 
-}); 
-
+nightToggle.addEventListener('click', event => {
+    body.classList.toggle('bg');
+})
