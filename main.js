3047519896acc4bc9f43e572efc6a3ecb140566f
Truija99 <truijam@gmail.com>
2018-10-10 12:42:51 +0300

    Lisatud tabeli näitamise ja peitmise funtsionaalsus.

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 // DOM Elements
 let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
-let poundOutput = document.getElementById('poundOutput')
-let ozOutput = document.getElementById('ozOutput')
+let poundOutput = document.getElementById('poundOutput');
+let ozOutput = document.getElementById('ozOutput');
+let output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
 
 
  // Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
+
+    output.style.visibility = 'visible';
+    if (kg === '0') {
+    	output.style.visibility = 'hidden'; 
+    } else {
+    	output.style.visibility = 'visible';
+    }
+
      gramOutput.innerHTML = `${kg * 1000} g`;
      poundOutput.innerHTML = `${kg * 2.20462262} lb`;
      ozOutput.innerHTML = `${kg * 35.2739619} oz`;
