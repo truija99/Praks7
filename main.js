9245e7c71281535b9771f29c1da368f673084622
Truija99 <truijam@gmail.com>
2018-10-10 12:33:11 +0300

    Lisatud naelade ja untside kalkuleerimise võimalus.

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // DOM Elements
 let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
+let poundOutput = document.getElementById('poundOutput')
+let ozOutput = document.getElementById('ozOutput')
+
 
  // Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
      gramOutput.innerHTML = `${kg * 1000} g`;
+     poundOutput.innerHTML = `${kg * 2.20462262} lb`;
+     ozOutput.innerHTML = `${kg * 35.2739619} oz`;
 }); 
+
